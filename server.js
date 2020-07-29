4e664613468a8ae4243e2c9eb05847a9e123b372
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-07-29 23:58:59 +0200

    Storing ids instead of names

--- server.js
+++ server.js
@@ -8,44 +8,66 @@ client.on("error", function (err) {
   console.log("Error " + err);
 });
 
+function generateId() {
+  const elementsSet = "0123456789abcdefghijklmnopqrstuvwxyz";
+  const idSize = 10;
+  let id = "";
+  for (let i = 0; i < idSize; i++) {
+    let index = Math.floor(Math.random() * elementsSet.length);
+    let currentElement = elementsSet.charAt(index);
+    id += currentElement;
+  }
+
+  return id;
+}
+
 app.post("/users", function (req, res) {
-  client.smembers("users", function (err, users) {
+  client.hgetall("users", function (err, users) {
     if (err) {
-      res.send("Error getting users.");
+      res.send("#Error getting users.");
     } else {
-      res.send(users);
+      res.send(Object.keys(users));
     }
   });
 });
 
 app.post("/users/register/:name", function (req, res) {
   const name = req.params.name;
-  client.sadd("users", name, function (err, isAdded) {
+  const id = generateId();
+
+  client.hexists("users", id, function (err, exists) {
     if (err) {
-      res.send("Error trying to register user.");
+      res.send("#Could not check if user id is already used.");
+      return;
+    }
+    if (exists) {
+      res.send("#User id is already used.");
+      return;
+    }
+  });
+
+  client.hset("users", id, name, function (err) {
+    if (err) {
+      res.send("#Error trying to register user.");
     } else {
-      if (isAdded) {
-        res.send("Done registering user.");
-      } else {
-        res.send("User is already registered.");
-      }
+      res.send(id);
     }
   });
 });
 
-app.post("/users/disconnect/:name", function (req, res) {
-    const name = req.params.name;
-    client.srem("users", name, function (err, isRemoved) {
-        if (err) {
-            res.send("Error trying to disconnect user.");
-          } else {
-            if (isRemoved) {
-              res.send("Done disconnecting user.");
-            } else {
-              res.send("User is already disconnected.");
-            }
-          }
-    });
+app.post("/users/disconnect/:id", function (req, res) {
+  const id = req.params.id;
+  client.hdel("users", id, function (err, isRemoved) {
+    if (err) {
+      res.send("#Error trying to disconnect user.");
+    } else {
+      if (isRemoved) {
+        res.send("Done disconnecting user.");
+      } else {
+        res.send("#It is not a registered user.");
+      }
+    }
+  });
 });
 
 app.listen(PORT, function () {
