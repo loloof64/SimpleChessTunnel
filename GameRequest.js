8dd84a5c0d92c85d341134d3fd7d7c70cefb2410
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-07-30 18:34:39 +0200

    We can get all requests targeting a user.

--- GameRequest.js
+++ GameRequest.js
@@ -1,5 +1,12 @@
 function setupGameRequestFeatures(app, db) {
   app.post("/game/new", function (req, res) {
+    if ([null, undefined].includes(req.body)) {
+      res
+        .status(422)
+        .send("You forgot to provide a value in the request body.");
+      return;
+    }
+
     const { emitterId, recipientName, emitterShouldHaveWhite } = req.body;
 
     // Checking that no parameter is missing
@@ -10,7 +17,7 @@ function setupGameRequestFeatures(app, db) {
       res
         .status(422)
         .send("You forgot to provide a value in the request body.");
-        return;
+      return;
     }
 
     let filter, document;
@@ -40,7 +47,11 @@ function setupGameRequestFeatures(app, db) {
               .send("Error while checking for the recipient in the database.");
             return;
           } else if ([null, undefined].includes(result)) {
-            res.status(400).send("Recipient is not registered or is the same as the emitter.");
+            res
+              .status(400)
+              .send(
+                "Recipient is not registered or is the same as the emitter."
+              );
             return;
           } else {
             // Creating the request
@@ -69,13 +80,55 @@ function setupGameRequestFeatures(app, db) {
                   emitterName: resultObj.emitterName,
                   recipientName: resultObj.recipientName,
                   recipientShouldHaveWhite: resultObj.recipientShouldHaveWhite,
-                  date
+                  date,
                 };
                 res.send(responseObj);
               }
             });
           }
         });
+      }
+    });
+  });
+
+  app.post("/requests/all", function (req, res) {
+    if ([null, undefined].includes(req.body)) {
+      res
+        .status(422)
+        .send("You forgot to provide a value in the request body.");
+      return;
+    }
+
+    const { serverId } = req.body;
+
+    if ([serverId].includes(undefined)) {
+      res
+        .status(422)
+        .send("You forgot to provide a value in the request body.");
+      return;
+    }
+
+    let filter;
+
+    // Checking that user exists
+    filter = { serverId: { $eq: serverId } };
+    db.collection("users").findOne(filter, function (error, result) {
+      if (error) {
+        res.status(500).send("Error while trying to check that user exists");
+      } else {
+        // Getting requests
+        filter = { recipientId: { $eq: serverId } };
+        db
+          .collection("pendingrequests")
+          .find(filter)
+          .toArray(function (err, result) {
+            if (err) {
+              res.status(500).send("Error while reading results");
+              return;
+            } else {
+              res.send(result);
+            }
+          });
       }
     });
   });
