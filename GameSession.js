4a15e9a9df1a4973b92d385d27a134159b15da19
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-08-02 18:17:41 +0200

    Added an endpoint for adding a move to a game.

--- GameSession.js
+++ GameSession.js
@@ -1,5 +1,5 @@
 const generateId = require("./utils").generateId;
-const {startPosition} = require('./chessLogic');
+const { startPosition, isWhiteTurn, doMove } = require("./chessLogic");
 
 function setupGameSessionFeatures(app, db) {
   app.post("/game/accept", function (req, res) {
@@ -52,42 +52,175 @@ function setupGameSessionFeatures(app, db) {
                 "Error while trying to delete the game request from the database."
               );
             return;
-          }
-          else {
-              const ownId = generateId();
-              let whiteId, blackId;
-              if (["true", true].includes(gameData.recipientShouldHaveWhite))
-              {
-                  whiteId = gameData.recipientId;
-                  blackId = gameData.emitterId;
+          } else {
+            const ownId = generateId();
+            let whiteId, blackId;
+            if (["true", true].includes(gameData.recipientShouldHaveWhite)) {
+              whiteId = gameData.recipientId;
+              blackId = gameData.emitterId;
+            } else {
+              whiteId = gameData.emitterId;
+              blackId = gameData.recipientId;
+            }
+            const status = "InProgress";
+            const startDate = Date.now();
+            const history = [];
+            const currentPosition = startPosition();
+            document = {
+              ownId,
+              whiteId,
+              blackId,
+              status,
+              startDate,
+              history,
+              currentPosition,
+            };
+            db.collection("gamesessions").insertOne(document, function (
+              error,
+              result
+            ) {
+              if (error) {
+                res
+                  .status(500)
+                  .send("Error while trying to create game session.");
+              } else {
+                res.send(result.ops[0]);
               }
-              else {
-                  whiteId = gameData.emitterId;
-                  blackId = gameData.recipientId;
-              }
-              const status = "InProgress";
-              const startDate = Date.now();
-              const history = [];
-              const currentPosition = startPosition();
-              document = {
-                ownId,
-                whiteId,
-                blackId,
-                status,
-                startDate,
-                history,
-                currentPosition,
-              };
-              db.collection("gamesessions").insertOne(document, function(error, result) {
-                    if (error) {
-                        res.status(500).send("Error while trying to create game session.");
-                    }
-                    else {
-                        res.send(result.ops[0]);
-                    }
-              });
+            });
           }
         });
+      }
+    });
+  });
+
+  app.post("/game/move", function (req, res) {
+    if ([null, undefined].includes(req.body)) {
+      res
+        .status(422)
+        .send("You forgot to provide a value in the request body.");
+      return;
+    }
+
+    const {
+      sessionId,
+      playerId,
+      startFile,
+      startRank,
+      endFile,
+      endRank,
+      promotion,
+    } = req.body;
+
+    if (
+      [sessionId, playerId, startFile, startRank, endFile, endRank].includes(
+        undefined
+      )
+    ) {
+      res
+        .status(422)
+        .send("You forgot to provide a value in the request body.");
+      return;
+    }
+
+    let filter, document;
+    filter = { ownId: { $eq: sessionId } };
+
+    db.collection("gamesessions").findOne(filter, function (error, resultGet) {
+      if (error) {
+        res
+          .status(500)
+          .send("Error while checking for the game session in the database.");
+        return;
+      } else if ([null, undefined].includes(resultGet)) {
+        res.status(400).send("Game session is not registered");
+        return;
+      } else {
+        if (![resultGet.whiteId, resultGet.blackId].includes(playerId)) {
+          res.status(401).send("You are not a player of the game.");
+          return;
+        } else {
+          const gameTurnIsWhite = isWhiteTurn(resultGet.currentPosition);
+          const isPlayerInTurn =
+            (gameTurnIsWhite && resultGet.whiteId === playerId) ||
+            (!gameTurnIsWhite && resultGet.blackId === playerId);
+
+          const startFileNum = parseInt(startFile);
+          const startRankNum = parseInt(startRank);
+          const endFileNum = parseInt(endFile);
+          const endRankNum = parseInt(endRank);
+
+          const wrongCoordinatesFormat =
+            isNaN(startFileNum) ||
+            isNaN(startRankNum) ||
+            isNaN(endFileNum) ||
+            isNaN(endRankNum);
+
+          const notInRangeCoordinates =
+            startFile < 0 ||
+            startFile > 7 ||
+            endFile < 0 ||
+            endFile > 7 ||
+            startRank < 0 ||
+            startRank > 7 ||
+            endRank < 0 ||
+            endRank > 7;
+          if (resultGet.status === "Ended") {
+            res.status(401).send("Game is already ended");
+            return;
+          } else if (!isPlayerInTurn) {
+            res.status(401).send("You are not the player in turn.");
+            return;
+          } else if (wrongCoordinatesFormat) {
+            res.status(422).send("Bad move coordinates format.");
+            return;
+          } else if (notInRangeCoordinates) {
+            res
+              .status(422)
+              .send("Some coordinates are not in board range [0-7].");
+            return;
+          } else {
+            const moveData = {
+              startFile: startFileNum,
+              startRank: startRankNum,
+              endFile: endFileNum,
+              endRank: endRankNum,
+              promotion: promotion,
+            };
+            const position = resultGet.currentPosition;
+            const moveToCommit = doMove(position, moveData);
+            if (!moveToCommit) {
+              res.status(422).send("Illegal move.");
+              return;
+            } else {
+              document = resultGet;
+              document.history.push(moveToCommit.moveSan);
+              document.currentPosition = moveToCommit.position;
+              document.status = moveToCommit.status;
+
+              db.collection("gamesessions").updateOne(
+                filter,
+                { $set: document },
+                function (error, resultUpdate) {
+                  if (error) {
+                    console.log(error);
+                    res
+                      .status(500)
+                      .send(
+                        "Error while trying to update the the game session in the database."
+                      );
+                    return;
+                  } else {
+                    const resultToSend = {
+                      san: moveToCommit.moveSan,
+                      status: moveToCommit.status,
+                    };
+                    res.send(resultToSend);
+                  }
+                }
+              );
+            }
+          }
+        }
       }
     });
   });
--- chessLogic.js
+++ chessLogic.js
@@ -5,6 +5,60 @@ function startPosition() {
     return chess.fen();
 }
 
+function isWhiteTurn(position) {
+    const chess = new Chess(position);
+    return chess.turn() === 'w';
+}
+
+function coordsAsAlgebraic(file, rank) {
+    let result = "";
+    result += String.fromCharCode("a".charCodeAt(0) + file);
+    result += String.fromCharCode("1".charCodeAt(0) + rank);
+
+    return result;
+}
+
+function getMove(position, moveData) {
+    const {startFile, startRank, endFile, endRank, promotion} = moveData;
+    const chess = new Chess(position);
+    const from = coordsAsAlgebraic(startFile, startRank);
+    const to = coordsAsAlgebraic(endFile, endRank);
+    let moveToDo = {
+        from, to
+    };
+    if (promotion)
+    {
+        moveToDo = {...moveToDo, promotion: promotion.toLowerCase()};
+    }
+    
+    const moveToCommit = chess.move(moveToDo);
+    const InProgressStatus = "InProgress";
+    const EndedStatus = "Ended";
+
+    let status = InProgressStatus;
+    if (chess.in_checkmate()) status = EndedStatus;
+    if (chess.in_stalemate()) status = EndedStatus;
+    if (chess.in_threefold_repetition()) status = EndedStatus;
+    if (chess.in_draw()) status = EndedStatus;
+
+    if (moveToCommit) {
+        return {
+            moveSan: moveToCommit.san,
+            position: chess.fen(),
+            status,
+        }
+    }
+    else {
+        return undefined;
+    }
+}
+
+function doMove(position, moveData) {
+    return getMove(position, moveData);
+}
+
 module.exports = {
     startPosition,
+    isWhiteTurn,
+    doMove,
 };
